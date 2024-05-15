260cee548a8d308246664e8d14f4e3449eb05695
LeVente Berry <berrylevente@gmail.com>
2024-05-14 20:58:16 -0400

    update server.js file struture

--- server/server.js
+++ server/server.js
@@ -1,12 +1,19 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('../client/dist'));
+// Serve static files from the client/dist directory
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+// Middleware for parsing application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
+
+// Middleware for parsing application/json
 app.use(express.json());
 
+// HTML routes
 require('./routes/htmlRoutes')(app);
 
 app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
